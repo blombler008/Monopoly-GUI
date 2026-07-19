898cda2ed1c5c7cc64ce506bfa38953376767b1e
blombler008 <37677225+blombler008@users.noreply.github.com>
2026-07-19 17:18:48 +0200

    Implement language observer callback
    Added a language observer callback to handle language changes.

--- src/Monopoly.c
+++ src/Monopoly.c
@@ -11,7 +11,8 @@
 /*********************
  *      DEFINES
  *********************/
-
+static void language_observer_cb(lv_observer_t * observer, lv_subject_t * subject);
+ 
 /**********************
  *      TYPEDEFS
  **********************/
@@ -28,6 +29,7 @@
  *      MACROS
  **********************/
 
+
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
@@ -37,8 +39,27 @@ void Monopoly_init(const char * asset_path)
     Monopoly_init_gen(asset_path);
 
     /* Add your own custom code here if needed */
+
+    lv_subject_add_observer(&language, language_observer_cb, NULL);
 }
 
+static void language_observer_cb(lv_observer_t * observer, lv_subject_t * subject)
+{
+    int32_t language = lv_subject_get_int(subject);
+    
+    switch (language)
+    {
+    case 0:
+        lv_translation_set_language("en");
+        break;
+    case 1:
+        lv_translation_set_language("de");
+        break;
+    default:
+        lv_translation_set_language("en");
+        break;
+    }
+}
 /**********************
  *   STATIC FUNCTIONS
  **********************/
